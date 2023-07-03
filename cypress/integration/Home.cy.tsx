61e9b2783487b15e53611addcb46c23a164392dc
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-07-03 21:35:55 +0200

    Wrap app test with provider

--- cypress/integration/Home.cy.tsx
+++ cypress/integration/Home.cy.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { mount } from "cypress/react18";
 
 import Home from "../../src/pages/Home/Home";
+import { UserContextProvider } from "../../src/context/UserContext";
 
 const selectors = {
   container: "[data-testid='home_container']",
@@ -11,7 +12,11 @@ const selectors = {
 describe("Home", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    mount(<Home />);
+    mount(
+      <UserContextProvider>
+        <Home />
+      </UserContextProvider>
+    );
     cy.get(selectors.container).should("be.visible");
   });
 });
